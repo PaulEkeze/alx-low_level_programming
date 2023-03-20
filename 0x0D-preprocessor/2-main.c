079a024a6b1c3578ca3fa3d04f38d32fc20b9bd3
PaulEkeze <paulekeze9@gmail.com>
2023-03-20 16:33:19 -0700

    Preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * Main - To print the name of a file, followed by a new line
@@ -9,6 +10,6 @@
 
 int main(void)
 {
-	printf("%s\n", __FILE__ );
+	printf("%s\n", __FILE__);
 	return (0);
 }
